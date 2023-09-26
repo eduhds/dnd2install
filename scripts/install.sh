7a4a6971c9a33c3cabf6d20d4761b616bbca2b67
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2023-09-26 08:07:57 -0300

    fix: install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-sudo cp ../build/app_installer /usr/local/bin
-sudo cp ../app_installer.png /usr/local/share/icons
-sudo cp ../app_installer.desktop /usr/share/applications
+sudo cp build/app_installer /usr/local/bin
+sudo cp app_installer.png /usr/local/share/icons
+sudo cp app_installer.desktop /usr/share/applications
 sudo chmod +x /usr/local/bin/app_installer
