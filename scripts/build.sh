6ceb4d79d517611318d6c4807cd44f0f65ee09db
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2023-10-17 14:37:11 +0000

    fix: build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 rm -rf build
 mkdir build
 
-g++ -g -static-libgcc -static-libstdc++ -std=c++17 \
+g++ *.cc \
+    -g -static-libgcc -static-libstdc++ -std=c++17 \
     -Ilibs/webview $(pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0) \
-    *.cc \
     -o build/dnd_installer
