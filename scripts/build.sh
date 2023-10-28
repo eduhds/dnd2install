322af186eafb1021ba2c78d0447345d18f5b0dcb
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2023-10-28 15:02:10 +0000

    fix: build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,38 @@
 #!/bin/sh
 
-rm -rf build
-mkdir -p build dist
-
 out_name="$(basename $(pwd))"
 
-g++ *.cc \
-    -g -static-libgcc -static-libstdc++ -std=c++17 \
-    -Ilibs/webview $(pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0) \
-    -o "build/$out_name"
+if [ "$1" = "-r" ]; then
+    # Build for release
 
-tar -czvf "dist/$out_name.tgz" "build/$out_name"
+    mkdir -p build
+    rm -rf build/release && mkdir build/release
+
+    g++ *.cc \
+        -static-libgcc -static-libstdc++ -std=c++17 \
+        -Ilibs/webview $(pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0) \
+        -o "build/release/$out_name"
+
+    # Build AppImage
+    rm -rf dist
+    mkdir dist
+    
+    appimage-builder --recipe AppImageBuilder.yml --skip-test
+
+    mv *.AppImage* dist
+elif [ "$1" = "-d" ]; then
+    # Build for debug
+    mkdir -p build
+    rm -rf build/debug && mkdir build/debug
+
+    g++ *.cc \
+        -g -std=c++17 \
+        -Ilibs/webview $(pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0) \
+        -o "build/debug/$out_name"
+
+    # Example for use as argument to test
+    tar -czvf "build/debug/$out_name.tgz" "build/debug/$out_name"
+else
+    echo "Usage: $0 [-r] [-d]"
+    exit 1
+fi
